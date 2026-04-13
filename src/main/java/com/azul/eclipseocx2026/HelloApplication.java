2bd2014b11220901ea902b634db8d894eff0bb80
Luqman Saeed <luqman.saeed@payara.fish>
2026-04-13 09:34:58 +0000

    Initial Jakarta EE 11 WAR skeleton for Payara 7

---
+++ src/main/java/com/azul/eclipseocx2026/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.azul.eclipseocx2026;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
